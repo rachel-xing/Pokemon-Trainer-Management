25dcae2ce1600f127ce20cf61f01d90ecad6ea72
Rachel Shing <xing0023@algonquinlive.com>
2025-07-02 17:09:23 -0400

    Updated package.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port:3000
+    host: '0.0.0.0',
+    port:process.env.PORT ? parseInt(process.env.PORT) : 3000,
   },
   base: ''
 })
