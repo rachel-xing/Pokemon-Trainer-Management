b7b60f823bfff207c48fc923254b23b721bb502d
Rachel Shing <xing0023@algonquinlive.com>
2025-07-02 16:58:24 -0400

    Build React Project

--- frontend/src/api/apiClient.jsx
+++ frontend/src/api/apiClient.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'https://pokemon-trainer-management.onrender.com/api',
     timeout: 10000,  // 24h
     headers: {
         'Content-Type': 'application/json',
